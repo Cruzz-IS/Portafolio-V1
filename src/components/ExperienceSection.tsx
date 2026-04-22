95c0771380e1d25bd6dc48133cc3e71537695ee8
cruz-IS <edwarcruz2000@yahoo.com>
2026-04-21 23:55:31 -0600

    actualizacion de la seccion de experiencia

--- src/components/ExperienceSection.tsx
+++ src/components/ExperienceSection.tsx
@@ -5,49 +5,88 @@ export default function ExperienceSection() {
     {
       title: "Feb 2025 - Jul 2025",
       content: (
-        // <div></div>
         <div>
           <h1 className="mb-8 sm:text-md md:text-2xl lg:text-3xl font-bold text-neutral-800 dark:text-neutral-200">
             Sercom de Honduras
           </h1>
-          <div className="grid grid-cols-2 gap-4">
-            <img
-              src="https://assets.aceternity.com/templates/startup-1.webp"
-              alt="startup template"
-              width={500}
-              height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
-            />
-            <img
-              src="https://assets.aceternity.com/templates/startup-2.webp"
-              alt="startup template"
-              width={500}
-              height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
-            />
-            <img
-              src="https://assets.aceternity.com/templates/startup-3.webp"
-              alt="startup template"
-              width={500}
-              height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
-            />
-            <img
-              src="https://assets.aceternity.com/templates/startup-4.webp"
-              alt="startup template"
-              width={500}
-              height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
-            />
+          <div className="grid grid-cols-1 gap-4">
+            <div>
+              <p>
+                ● Desarrollé e implementé procesos ETL utilizando Talend Open
+                Studio para la extracción, transformación y carga de datos desde
+                múltiples fuentes hacia distintas bases de datos.
+              </p>
+            </div>
+            <div>
+              <p>
+                ● Gestioné bases de datos Oracle Database y MySQL, asegurando la
+                integridad y disponibilidad de la información.
+              </p>
+            </div>
+            <div>
+              <p>
+                ● Generé reportes analíticos y de configuración de redes de
+                datos para el área de Ingeniería de Clientes Corporativos
+                utilizando Excel y consultas SQL avanzadas.
+              </p>
+            </div>
           </div>
         </div>
+        // <div className="grid grid-cols-2 gap-4">
+        //   <img
+        //     src="https://assets.aceternity.com/templates/startup-1.webp"
+        //     alt="startup template"
+        //     width={500}
+        //     height={500}
+        //     className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+        //   />
+        //   <img
+        //     src="https://assets.aceternity.com/templates/startup-2.webp"
+        //     alt="startup template"
+        //     width={500}
+        //     height={500}
+        //     className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+        //   />
+        //   <img
+        //     src="https://assets.aceternity.com/templates/startup-3.webp"
+        //     alt="startup template"
+        //     width={500}
+        //     height={500}
+        //     className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+        //   />
+        //   <img
+        //     src="https://assets.aceternity.com/templates/startup-4.webp"
+        //     alt="startup template"
+        //     width={500}
+        //     height={500}
+        //     className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+        //   />
+        // </div>
       ),
     },
     {
       title: "Ene 2021 - Dic 2024",
       content: (
         <div>
-
+          <h1 className="mb-8 sm:text-md md:text-2xl lg:text-3xl font-bold text-neutral-800 dark:text-neutral-200">
+            APROTEC
+          </h1>
+          <div className="grid grid-cols-1 gap-4">
+            <div>
+              <p>
+                ● Instalé y operé equipos de audio, iluminación DMX (MyDmx y
+                Onyx) y pantallas led (Resolume Arena) para más de 100 eventos
+                corporativos, conciertos, bodas y celebraciones.
+              </p>
+            </div>
+            <div>
+              <p>
+                ● Brindé soporte técnico especializado en tiempo real,
+                manteniendo una tasa de éxito del 95% en la ejecución de eventos
+                sin interrupciones técnicas.
+              </p>
+            </div>
+          </div>
         </div>
         // <div>
         //   <h1 className="mb-8 sm:text-md md:text-2xl lg:text-3xl font-bold text-neutral-800 dark:text-neutral-200">
@@ -93,7 +132,27 @@ export default function ExperienceSection() {
     {
       title: "Jul 2019 - May 2020",
       content: (
-        <div></div>
+        <div>
+          <h1 className="mb-8 sm:text-md md:text-2xl lg:text-3xl font-bold text-neutral-800 dark:text-neutral-200">
+            OPS Contact Center
+          </h1>
+          <div className="grid grid-cols-1 gap-4">
+            <div>
+              <p>
+                ● Gestioné cobros de más de 30 cuentas mensuales de planes
+                móviles y residenciales vía Call Center. Contribuí al
+                cumplimiento de la meta mensual de recuperación de cartera
+                mediante seguimiento y negociación efectiva con clientes.
+              </p>
+            </div>
+            <div>
+              <p>
+                ● Brindé atención al cliente manteniendo un índice de
+                satisfacción superior al 90%.
+              </p>
+            </div>
+          </div>
+        </div>
         // <div>
         //   <h1 className="mb-8 sm:text-md md:text-2xl lg:text-3xl font-bold text-neutral-800 dark:text-neutral-200">
         //     OPS Contact Center
